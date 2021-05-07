99ec97bd8a26ac10c013a10da3d334a33afde86a
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-06 22:46:05 -0400

    setting state and handling input change for search

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,35 @@ import React from 'react'
 import Navbar from './Navbar'
 
 class Home extends React.Component {
+
+    state={
+        search: ''
+    }
+
+    handleChange = (event) => {
+
+        this.setState({
+            search: event.target.value
+        })
+
+    }
     render(){
+    
         return(
             <div className="flex min-h-screen">  
                 <Navbar />
                 <div className='flex flex-col w-full'>
-                <header className='flex justify-end bg-blue-300 p-2'>
-            <form>
-              <input className='rounded-lg pl-2 mr-2' type='text' value='' placeholder='Search'/>
-              <button className='w-20 px-2 rounded-md bg-yellow-400' type='submit' value='Search'>Search</button>
-            </form>
-          </header>
-                <div className='text-center'>
-                    Welcome!
+                    <header className='flex justify-end bg-blue-300 p-2'>
+                        <form>
+                            <input onChange={this.handleChange} className='rounded-l-md pl-2' type='text' value='search' placeholder='Search'/>
+                            <button className='w-20 px-2 rounded-r-md bg-yellow-400' type='submit' value='Search'>Search</button>
+                        </form>
+                    </header>
+                    <div className='text-center'>
+                        Welcome!
+                    </div>
                 </div>
-                </div>
-                </div>
+            </div>
         )
     }
 }
